15c239a04e7b2390d868f6a048509799bd84eeca
Athar <atharmd777@gmail.com>
2021-07-23 14:35:08 +0530

    header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menuItems : any[]= []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.createMenuItem();
+  }
+
+  createMenuItem(){
+
+    let item = {
+      title:"myTitle",
+      link:"mylink",
+      url:"myurl"
+    }
+
+    for(let i =0; i <= 5; i++){
+        console.log(i);
+        this.menuItems.push(item)
+    }
+
+
   }
 
 }
